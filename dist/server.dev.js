8bce401705b6d287033390347e2ad895a00a46aa
Jose EduardoLuciano da Silva <jose.silva@ws.wsys.inf.br>
2020-11-18 11:18:11 -0300

    TEste

---
+++ dist/server.dev.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var express = require('express');
+
+var app = express();
+
+var bodyParser = require("body-parser");
+
+var cors = require('cors');
+
+var fs = require('fs');
+
+require('dotenv').config();
+
+app.use(bodyParser.json({
+  limit: '50mb',
+  extended: true,
+  type: '*/*'
+}));
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true
+}));
+app.use(cors());
+/*app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:8080']
+}))*/
+
+var content = require('./api');
+
+content(app, fs);
+var porta = process.env.PORT || 87;
+app.listen(porta, function () {
+  console.log("Rodando a porta ".concat(porta));
+});
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-var http = require('http');
 var express = require('express');
 var app = express()
-//var server = http.Server(app)
 var bodyParser = require("body-parser")
-//var cors = require('cors')
+var cors = require('cors')
 var fs = require('fs');
 
 require('dotenv').config()
 
-app.use(bodyParser.json({ limit: '50mb', extended: true, type: '*/*' }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(bodyParser.json({
+    limit: '50mb',
+    extended: true,
+    type: '*/*'
+}))
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true
+}))
+app.use(cors());
 /*app.use(cors({
   credentials: true,
   origin: ['http://localhost:8080']
@@ -19,7 +25,6 @@ var content = require('./api');
 content(app, fs);
 
 const porta = process.env.PORT || 87;
-app.listen(porta, () =>{
-  console.log(`Rodando a porta ${porta}`)
-})
-//server.listen(porta);
+app.listen(porta, () => {
+    console.log(`Rodando a porta ${porta}`)
+});
